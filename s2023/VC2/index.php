4d5215a289252667668be7c65df93fc59fcf3598
hinal <hinalpanchal70@gmail.com>
2023-08-26 13:41:05 +0530

    project

---
+++ s2023/VC2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Video Calling Page</title>
+</head>
+<body>
+    <div class="video-container">
+        <div class="video-window" id="localVideo">
+            ndkvdkv
+            <video></video>
+            <!-- Local Video Stream (Your Camera) -->
+        </div>
+        <div class="video-window" id="remoteVideo">
+            <!-- Remote Video Stream (Call Partner) -->
+            jkjfsdfc
+            <video></video>
+        </div>
+    </div>
+</body>
+</html>
